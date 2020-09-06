d76debb09456c59baac83b7875fa733300ee9ed2
yopaseopor <yopaseopor@gmail.com>
2020-09-07 00:30:50 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -841,7 +841,7 @@ var config = {
 			query: '(way[highway=motorway]["name:1975"]({{bbox}});node(w);way[highway=trunk]["name:1975"]({{bbox}});node(w);way[highway=primary]["name:1975"]({{bbox}});node(w);way[highway=secondary]["name:1975"]({{bbox}});node(w);way[highway=tertiary]["name:1975"]({{bbox}});node(w);way[highway=unclassified]["name:1975"]({{bbox}});node(w);way[highway=track]["name:1975"]({{bbox}});node(w);way[highway=residential]["name:1975"]({{bbox}});node(w);way[highway=service]["name:1975"]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'icones/maxspeed.svg',
 			style: function (feature) {
-				var maxspeed = feature.get('maxspeed') || '';
+				var maxspeed = feature.get('"name:1975"') || '';
 				if (maxspeed === ''){
 					return undefined;
 				}
