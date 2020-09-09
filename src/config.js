370cec6aa1647f797eada654db0e5764e6c57c7e
yopaseopor <yopaseopor@gmail.com>
2020-09-09 15:45:50 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1152,12 +1152,12 @@ var config = {
 		},
 		{
 			group: 'Històric',
-			title: '1975',
-			query: '(nwr[~"^name:197.$"~".*"]({{bbox}});node(w););out meta;',
+			title: '1976',
+			query: '(nwr[~"^name(:.*)?$"~"."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
 			iconStyle: 'background-color:#714601',
 			style: function (feature) {
-				var name = feature.get('~"^name\:197.$"~".*"') || '';
+				var name = feature.get('~"^name(:.*)?$"~"."') || '';
 				var styles = {
 					'amenity': {
 						'parking': new ol.style.Style({
