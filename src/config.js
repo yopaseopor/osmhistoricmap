85cc4d1d6b3d521088ab24480d20ad0af166fffe
yopaseopor <yopaseopor@gmail.com>
2020-09-19 02:19:55 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /**
  * OSM Cat config
  */
- 
+ {
 Feature.prototype.re_get = function(r) {
     var k = this.getKeys().filter(function(t){return t.match(key_regex)}).pop()
     return this.get(k) || ''
