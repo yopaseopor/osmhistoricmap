fa4cc2e7643beebfa89e6db1aa44865b0b05c743
yopaseopor <yopaseopor@gmail.com>
2020-09-20 13:52:45 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -746,6 +746,35 @@ var config = {
 		},
 		{
 			group: 'Per date',
+			title: 'Interval of years2',
+			query: '(nwr[~"^name:....-....$"~"."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#0000ff',
+			style: function () {
+				var fill = new ol.style.Fill({
+					color: 'rgba(0,0,255,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#0000ff',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+							text: new ol.style.Text({
+								text: name
+							}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
+		{
+			group: 'Per date',
 			title: 'Exact Interval of years',
 			query: '(nwr[~"^name:....-..-..-....-..-..$"~"."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
