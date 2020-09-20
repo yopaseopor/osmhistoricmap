f661e3813e03d80666ce3e0fe4213d11945eb54c
yopaseopor <yopaseopor@gmail.com>
2020-09-20 19:53:11 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -755,7 +755,7 @@ var config = {
 				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
 				var name = feature.get(name_key) || '';
 				var fill = new ol.style.Fill({
-					color: 'rgba(0,0,255,0.4)'
+					color: 'rgba(225,255,255,0.4)'
 				});
 				var stroke = new ol.style.Stroke({
 					color: '#e1ffff',
