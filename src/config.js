2052f5f8909e63e215cf4b674b7a3e9ff75b8863
yopaseopor <yopaseopor@gmail.com>
2020-10-01 23:24:47 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -70,7 +70,6 @@ var config = {
               source: new ol.source.GeoJSON({
                  projection : 'EPSG:4326',
                  url: 'mygeojson.json'
-				   })
            })
 			}),
 			visible: false
