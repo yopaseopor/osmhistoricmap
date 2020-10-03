7f51cdb3bbac52abe7d6252aad8ab3103a2b5d06
yopaseopor <yopaseopor@gmail.com>
2020-10-03 10:07:47 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -830,6 +830,43 @@ var config = {
 		},
 		{
 			group: 'Test',
+			title: '1961',
+			query: '(nwr[~"^name:197[0-9]$"~"."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#0000ff',
+  style: function (feature) {
+    style.getText().setText(feature.get('name'));
+    return style;
+  },
+});
+
+var vectorLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        format: new ol.format.GeoJSON(),
+        url: 'export.geojson'
+    }),
+
+				var stroke = new ol.style.Stroke({
+					color: '#0000ff',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+							text: new ol.style.Text({
+								text: name
+							}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
+		{
+			group: 'Test',
 			title: '1960',
 			query: '(nwr[~"^name:197[0-9]$"~"."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
