f67b053c4845aca43f1364458687ea8fb25f2b99
yopaseopor <yopaseopor@gmail.com>
2020-12-29 00:37:04 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2422,11 +2422,11 @@ var vectorLayer = new ol.layer.Vector({
 		{
 			group: 'Topics',
 			title: 'ES_1932-1939 II Spanish Republic',
-			query: '(nwr[~"^name:193[0-9]$"~"."]({{bbox}});node(w););out meta;',
+			query: '(nwr[~"^name:193[0-9]-193[0-9]$"~"."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
 			iconStyle: 'background-color:#714601',
 			style: function (feature) {
-				var key_regex = /^name:193[0-9]$/
+				var key_regex = /^name:193[0-9]-193[0-9]$/
 				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
 				var name = feature.get(name_key) || '';
 				var styles = {
