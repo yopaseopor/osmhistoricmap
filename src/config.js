800c6752a05b3c0fd5e0d2c524602c9ec9a16a0c
yopaseopor <yopaseopor@gmail.com>
2020-12-29 01:49:06 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2842,7 +2842,7 @@ var vectorLayer = new ol.layer.Vector({
 		{
 			group: 'Topics',
 			title: 'DE_1961-1989_11_09 Berliner Mauer',
-			query: '(nwr[~"^name:1961-1989_11_09$"~"."]({{bbox}});node(w););out meta;',
+			query: '(r(6651797)({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
 			iconStyle: 'background-color:#714601',
 			style: function (feature) {
