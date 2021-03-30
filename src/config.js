1ee554140444359330b6aed5992435e3c1d15f2c
yopaseopor <yopaseopor@gmail.com>
2021-03-31 01:04:03 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3153,11 +3153,11 @@ var vectorLayer = new ol.layer.Vector({
 		{
 			group: 'Topics',
 			title: 'DE_1961-1989_11_09 Berliner Mauer',
-			query: '(nwr[~"^name:196[0-9]-name:198[0-9]_11_09$"~"."]({{bbox}});node(w););out meta;',
+			query: '(nwr[~"^name:196[0-9]-198[0-9]_11_09$"~"."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
 			iconStyle: 'background-color:#714601',
 			style: function (feature) {
-				var key_regex = /^name:196[0-9]-name:198[0-9]_11_09$/
+				var key_regex = /^name:196[0-9]-198[0-9]_11_09$/
 				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
 				var name = feature.get(name_key) || '';
 				var styles = {
