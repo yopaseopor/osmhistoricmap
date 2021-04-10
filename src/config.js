a8b191ceb21b21f1c2e3bf0595679225d4b32989
yopaseopor <yopaseopor@gmail.com>
2021-04-10 21:33:53 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3079,7 +3079,22 @@ var vectorLayer = new ol.layer.Vector({
 								text: name
 							})
 						})
-										},
+					},
+					'amenity': {
+						'.*': new ol.style.Style({
+							zIndex: 100,
+							stroke: new ol.style.Stroke({
+								color: 'rgba(246, 99, 79, 1.0)',
+								width: 1
+							}),
+							fill: new ol.style.Fill({
+								color: 'rgba(246, 99, 79, 0.3)'
+							}),
+							text: new ol.style.Text({
+								text: name
+							})
+						})
+					},
 					'place': {
 						'.*': new ol.style.Style({
 							zIndex: 100,
