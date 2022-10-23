6ffece399722eb3ba4b260726852226b4891c215
yopaseopor <yopaseopor@gmail.com>
2022-10-23 23:32:34 +0200

    Inexact date

--- src/config.js
+++ src/config.js
@@ -924,6 +924,32 @@ var config = {
 				return style;
 			}
 		},
+				{
+			group: 'Per date',
+			title: 'Inexact start date',
+			query: '(nwr[~"^name:-....-..-..$"~"."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#ff0000',
+			style: function () {
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#ff0000',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
 		{
 			group: 'Per date',
 			title: 'Exact year',
@@ -936,6 +962,32 @@ var config = {
 				});
 				var stroke = new ol.style.Stroke({
 					color: '#0000ff',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
+		{
+			group: 'Per date',
+			title: 'Inexact start year',
+			query: '(nwr[~"^name:-....$"~"."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#ff0000',
+			style: function () {
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#ff0000',
 					width: 1.25
 				});
 				var style = new ol.style.Style({
