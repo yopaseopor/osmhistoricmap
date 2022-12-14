35d5d1a436610e59f976bac9d6a4a14c0a1f1854
yopaseopor <yopaseopor@gmail.com>
2022-12-14 22:36:37 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -46,7 +46,7 @@ var config = {
 			iconSrc: imgSrc + 'logo_hotosm.png',
 			source: new ol.source.XYZ({
 				attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
-				url: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+				url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
 			}),
 			visible: false
 		}),
