afa84fcab4bcf3c9c9931a39dfd3bf2956b23499
yopaseopor <yopaseopor@gmail.com>
2022-12-14 22:39:32 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -64,7 +64,7 @@ var config = {
 			iconSrc: imgSrc + 'logo_stadiamaps.svg',
 			source: new ol.source.XYZ({
 				attributions: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-				url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
+				url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png'
 			}),
 			visible: false
 		}),
