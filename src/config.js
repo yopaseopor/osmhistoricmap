629bbe1891497c07e0ee8f93f717cb97376ddd6e
yopaseopor <yopaseopor@gmail.com>
2023-04-22 17:41:59 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6162,8 +6162,8 @@ var vectorLayer = new ol.layer.Vector({
 		},
 		{
 			group: 'Topics',
-			title: 'ES_1939-1975 Spanish Dictatorship names_date.',
-			query: '(nwra[~"^name:19[4-7][0-9]$"~"."]({{bbox}});way(r)({{bbox}});node(w););out meta;',
+			title: 'ES_1939-1975 Spanish Dictatorship names_date',
+			query: '(nwr[~"^name:19[4-7][0-9]$"~"."]({{bbox}});way(r)({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'base/circle.svg',
 			iconStyle: 'background-color:#714601',
 			style: function (feature) {
